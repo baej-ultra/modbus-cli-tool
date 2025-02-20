17b766263187359325537ea470780d9c47f7e1f3
baej-ultra <pigmejov@gmail.com>
2025-02-20 11:55:51 +0100

    support for functions 0x01, 0x02, 0x03, 0x04 for  server

--- src/main/java/org/baej/modbusclitool/modbus/server/ModbusServerService.java
+++ src/main/java/org/baej/modbusclitool/modbus/server/ModbusServerService.java
@@ -1,7 +1,6 @@
 package org.baej.modbusclitool.modbus.server;
 
 import com.digitalpetri.modbus.ExceptionCode;
-import com.digitalpetri.modbus.FunctionCode;
 import com.digitalpetri.modbus.exceptions.ModbusResponseException;
 import com.digitalpetri.modbus.exceptions.UnknownUnitIdException;
 import com.digitalpetri.modbus.pdu.*;
@@ -15,81 +14,105 @@ import java.util.Random;
 
 @Service
 public class ModbusServerService implements ModbusServices {
+
+    private final Random random = new Random();
+
     @Override
-    public ReadCoilsResponse readCoils(ModbusRequestContext context, int unitId, ReadCoilsRequest request) throws ModbusResponseException, UnknownUnitIdException {
-        return ModbusServices.super.readCoils(context, unitId, request);
+    public ReadCoilsResponse readCoils(ModbusRequestContext context, int unitId, ReadCoilsRequest request) throws ModbusResponseException {
+        var registers = getRandomBytes(request.getFunctionCode(), request.address(), (int) Math.ceil(request.quantity() / 8d));
+        return new ReadCoilsResponse(registers);
     }
 
     @Override
-    public ReadDiscreteInputsResponse readDiscreteInputs(ModbusRequestContext context, int unitId, ReadDiscreteInputsRequest request) throws ModbusResponseException, UnknownUnitIdException {
-        return ModbusServices.super.readDiscreteInputs(context, unitId, request);
+    public ReadDiscreteInputsResponse readDiscreteInputs(ModbusRequestContext context, int unitId, ReadDiscreteInputsRequest request) throws ModbusResponseException {
+        var registers = getRandomBytes(request.getFunctionCode(), request.address(), (int) Math.ceil(request.quantity() / 8d));
+        return new ReadDiscreteInputsResponse(registers);
     }
 
     @Override
-    public ReadHoldingRegistersResponse readHoldingRegisters(ModbusRequestContext context, int unitId, ReadHoldingRegistersRequest request) throws ModbusResponseException, UnknownUnitIdException {
-        Random random = new Random();
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-
-        // If request is bigger than buffer return Illegal Data Address error to client
-        if (request.address() >= buffer.capacity() / 2 || request.address() + request.quantity() > buffer.capacity() / 2) {
-            throw new ModbusResponseException(FunctionCode.READ_HOLDING_REGISTERS, ExceptionCode.ILLEGAL_DATA_ADDRESS);
-        }
-
-        for (int i = 0; i < 5; i++) {
-            buffer.putShort((short) random.nextInt(Short.MAX_VALUE + 1));
-        }
-        for (int i = 0; i < 5; i++) {
-            buffer.putInt(random.nextInt());
-        }
-        for (int i = 0; i < 5; i++) {
-            buffer.putLong(random.nextLong());
-        }
-        for (int i = 0; i < 5; i++) {
-            buffer.putFloat(random.nextFloat()*10);
-        }
-        for (int i = 0; i < 5; i++) {
-            buffer.putDouble(random.nextDouble()*100);
-        }
-
-        byte[] registers = buffer.array();
-        int start = request.address();
-        int end = request.address() + (request.quantity() * 2);
-
-        return new ReadHoldingRegistersResponse(Arrays.copyOfRange(registers, start, end));
+    public ReadHoldingRegistersResponse readHoldingRegisters(ModbusRequestContext context, int unitId, ReadHoldingRegistersRequest request) throws ModbusResponseException {
+        byte[] registers = getRandomRegisters(request.getFunctionCode(), request.address(), request.quantity());
+        return new ReadHoldingRegistersResponse(registers);
     }
 
     @Override
-    public ReadInputRegistersResponse readInputRegisters(ModbusRequestContext context, int unitId, ReadInputRegistersRequest request) throws ModbusResponseException, UnknownUnitIdException {
-        return ModbusServices.super.readInputRegisters(context, unitId, request);
-    }
-
-    @Override
-    public WriteSingleCoilResponse writeSingleCoil(ModbusRequestContext context, int unitId, WriteSingleCoilRequest request) throws ModbusResponseException, UnknownUnitIdException {
-        return ModbusServices.super.writeSingleCoil(context, unitId, request);
-    }
-
-    @Override
-    public WriteSingleRegisterResponse writeSingleRegister(ModbusRequestContext context, int unitId, WriteSingleRegisterRequest request) throws ModbusResponseException, UnknownUnitIdException {
-        return ModbusServices.super.writeSingleRegister(context, unitId, request);
-    }
-
-    @Override
-    public WriteMultipleCoilsResponse writeMultipleCoils(ModbusRequestContext context, int unitId, WriteMultipleCoilsRequest request) throws ModbusResponseException, UnknownUnitIdException {
-        return ModbusServices.super.writeMultipleCoils(context, unitId, request);
-    }
-
-    @Override
-    public WriteMultipleRegistersResponse writeMultipleRegisters(ModbusRequestContext context, int unitId, WriteMultipleRegistersRequest request) throws ModbusResponseException, UnknownUnitIdException {
-        return ModbusServices.super.writeMultipleRegisters(context, unitId, request);
-    }
-
-    @Override
-    public MaskWriteRegisterResponse maskWriteRegister(ModbusRequestContext context, int unitId, MaskWriteRegisterRequest request) throws ModbusResponseException, UnknownUnitIdException {
-        return ModbusServices.super.maskWriteRegister(context, unitId, request);
+    public ReadInputRegistersResponse readInputRegisters(ModbusRequestContext context, int unitId, ReadInputRegistersRequest request) throws ModbusResponseException {
+        byte[] registers = getRandomRegisters(request.getFunctionCode(), request.address(), request.quantity());
+        return new ReadInputRegistersResponse(registers);
     }
 
     @Override
     public ReadWriteMultipleRegistersResponse readWriteMultipleRegisters(ModbusRequestContext context, int unitId, ReadWriteMultipleRegistersRequest request) throws ModbusResponseException, UnknownUnitIdException {
         return ModbusServices.super.readWriteMultipleRegisters(context, unitId, request);
+    }
+
+    /*
+    Generate a bunch of random registers of various data types
+     */
+
+    private byte[] getRandomRegisters(int functionCode, int address, int quantity) throws ModbusResponseException {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        int lastAddress = buffer.capacity() / 2;
+
+        if (address >= 0 && address <= lastAddress) {
+            if (quantity >= 1 && quantity <= 125) {
+                if (address + quantity > lastAddress + 1) {
+                    throw new ModbusResponseException(functionCode, ExceptionCode.ILLEGAL_DATA_ADDRESS.getCode());
+                }
+            } else {
+                throw new ModbusResponseException(functionCode, ExceptionCode.ILLEGAL_DATA_VALUE.getCode());
+            }
+        } else {
+            throw new ModbusResponseException(functionCode, ExceptionCode.ILLEGAL_DATA_ADDRESS.getCode());
+        }
+
+        for (int i = 0; i < 5; i++) { // Short ints (addr 0-4)
+            buffer.putShort((short) random.nextInt(Short.MAX_VALUE + 1));
+        }
+        for (int i = 0; i < 5; i++) { // Ints (ddr 5-14)
+            buffer.putInt(random.nextInt());
+        }
+        for (int i = 0; i < 5; i++) { // Long ints (addr 15-34)
+            buffer.putLong(random.nextLong());
+        }
+        for (int i = 0; i < 5; i++) { // Floats/Reals (addr 35-44)
+            buffer.putFloat(random.nextFloat() * 10);
+        }
+        for (int i = 0; i < 5; i++) { // Double floats (addr 45-64)
+            buffer.putDouble(random.nextDouble() * 100);
+        }
+
+        byte[] registers = buffer.array();
+        int end = address + quantity * 2;
+        return Arrays.copyOfRange(registers, address, end);
+    }
+
+    private byte[] getRandomBytes(int functionCode, int address, int quantity) throws ModbusResponseException {
+        ByteBuffer buffer = ByteBuffer.allocate(3);
+
+        int lastAddress = buffer.capacity();
+
+        if (address >= 0 && address <= lastAddress) {
+            if (quantity >= 1 && quantity <= 3) {
+                if (address + quantity > lastAddress + 1) {
+                    throw new ModbusResponseException(functionCode, ExceptionCode.ILLEGAL_DATA_ADDRESS.getCode());
+                }
+            } else {
+                throw new ModbusResponseException(functionCode, ExceptionCode.ILLEGAL_DATA_VALUE.getCode());
+            }
+        } else {
+            throw new ModbusResponseException(functionCode, ExceptionCode.ILLEGAL_DATA_ADDRESS.getCode());
+        }
+
+        byte[] randomByte = new byte[1];
+        random.nextBytes(randomByte);
+        buffer.put((byte) 0);
+        buffer.put((byte) 0b11111111);
+        buffer.put(randomByte);
+
+        byte[] registers = buffer.array();
+        int end = address + quantity;
+        return Arrays.copyOfRange(registers, address, end);
     }
 }
